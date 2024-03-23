a6fee5be6c4597ac355b29702044d16161a51956
Zain Noman <zaina526@gmail.com>
2024-03-23 17:33:03 +0500

    kinda works gonna try resampling approach

---
+++ pulse_audio_implementation/pacat-simple.c
@@ -0,0 +1,233 @@
+/***
+  This file is part of PulseAudio.
+
+  PulseAudio is free software; you can redistribute it and/or modify
+  it under the terms of the GNU Lesser General Public License as published
+  by the Free Software Foundation; either version 2.1 of the License,
+  or (at your option) any later version.
+
+  PulseAudio is distributed in the hope that it will be useful, but
+  WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+  General Public License for more details.
+
+  You should have received a copy of the GNU Lesser General Public License
+  along with PulseAudio; if not, see <http://www.gnu.org/licenses/>.
+***/
+
+#ifdef HAVE_CONFIG_H
+#include <config.h>
+#endif
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <errno.h>
+#include <fcntl.h>
+#include <math.h>
+#include <time.h>
+
+#include <pulse/simple.h>
+#include <pulse/error.h>
+
+#include "wave.h"
+
+struct engineAudio
+{
+    int16_t *raw_audio;
+    float rpm;
+    int numSamples;
+    int currentIdx;
+    char *filename;
+    float gain;
+};
+
+#define SAMPLE_RATE 16000
+
+int EngineAudioLoadData(struct engineAudio *out)
+{
+    WaveFile *f = wave_open(out->filename, WAVE_OPEN_READ);
+    if (f == NULL)
+    {
+        printf("cant open file %s \n", out->filename);
+        return 1;
+    }
+
+    int sampleRate = wave_get_sample_rate(f);
+    if (sampleRate != SAMPLE_RATE)
+    {
+        printf("SAMPLERATE %d, %s \n", sampleRate, out->filename);
+        wave_close(f);
+        return 1;
+    }
+
+    int numSamples = wave_get_length(f);
+    if (wave_get_sample_size(f) != sizeof(int16_t))
+    {
+        printf("samplesize %ld \n", wave_get_sample_size(f));
+        wave_close(f);
+        return 1;
+    }
+
+    int16_t *raw_audio = malloc(sizeof(int16_t) * numSamples);
+    wave_read(f, raw_audio, numSamples);
+    wave_close(f);
+
+    out->numSamples = numSamples;
+    out->raw_audio = raw_audio;
+    out->currentIdx = 0;
+    return 0;
+}
+
+float randn()
+{
+    float retval = -6;
+    for (int i = 0; i < 12; i++)
+    {
+        retval += ((float)rand()) / RAND_MAX;
+    }
+    return retval;
+}
+
+int main(int argc, char *argv[])
+{
+
+    struct engineAudio audios[] = {
+        {.rpm = 25,
+         .filename = "../../alto_25Hz.wav",
+         .gain = 0.5},
+        {.rpm = 40,
+         .filename = "../../alto_40Hz.wav",
+         .gain = 0.5},
+        {.rpm = 55,
+         .filename = "../../alto_60Hz.wav",
+         .gain = 0.8},
+        {.rpm = 67,
+         .filename = "../../alto_65Hz.wav",
+         .gain = 1},
+    };
+    for (size_t i = 0; i < 4; i++)
+    {
+        if (EngineAudioLoadData(&audios[i]) != 0)
+        {
+            return 1;
+        }
+    }
+
+    /* The Sample format to use */
+    pa_sample_spec ss = {
+        .format = PA_SAMPLE_S16LE,
+        .rate = SAMPLE_RATE,
+        .channels = 1};
+
+    pa_simple *s = NULL;
+    int ret = 1;
+    int error;
+
+    /* Create a new playback stream */
+    if (!(s = pa_simple_new(NULL, argv[0], PA_STREAM_PLAYBACK, NULL, "playback", &ss, NULL, NULL, &error)))
+    {
+        fprintf(stderr, __FILE__ ": pa_simple_new() failed: %s\n", pa_strerror(error));
+        goto finish;
+    }
+
+    // crate a file to write audio to 
+    WaveFile* of = wave_open("out.wav",WAVE_OPEN_WRITE);
+    if (of == NULL){
+        printf("cant create out file \n");
+        return 1;
+    }
+    wave_set_format(of, WAVE_FORMAT_PCM);
+    wave_set_num_channels(of,1);
+    wave_set_sample_rate(of,SAMPLE_RATE);
+    wave_set_sample_size(of,sizeof(int16_t));
+
+#define MAXRPM 67
+#define MINRPM 25
+#define SIN_AMMOUNT 0.0
+    float current_rpm = MINRPM;
+    float sin_phase = 0;
+    int audioIdx = 0;
+    for (float t = 0; t < 14;)
+    {
+        int16_t buf[1024];
+        for (size_t i = 0; i < 1024; i++)
+        {
+#define STEP_SPEED 0.3 / SAMPLE_RATE
+            t += 1.0 / SAMPLE_RATE;
+            if (fmod(t, 7) > 3.5)
+                current_rpm += (MAXRPM - current_rpm) * (STEP_SPEED);
+            else
+                current_rpm += (MINRPM - current_rpm) * (STEP_SPEED);
+
+            current_rpm += randn() * 0.08;
+            if (current_rpm > MAXRPM)
+                current_rpm = MAXRPM;
+            else if (current_rpm < MINRPM)
+                current_rpm = MINRPM;
+
+            // audio index
+            if (current_rpm > audios[audioIdx + 1].rpm)
+            {
+                audioIdx++;
+                printf("audioIdx %d \n", audioIdx);
+            }
+            else if (current_rpm < audios[audioIdx].rpm)
+            {
+                audioIdx--;
+                printf("audioIdx %d \n", audioIdx);
+            }
+            
+            float lerp_factor = (current_rpm - audios[audioIdx].rpm) / (audios[audioIdx + 1].rpm - audios[audioIdx].rpm);
+
+            float freq = (current_rpm*1.5) * 2.0 * 3.1415 / SAMPLE_RATE; // SAMPLE_RATE (HZ) = 2*pi, 1HZ = 2*pi/SAMPLE_RATE
+            sin_phase += freq;
+            // buf[i] = (double)(raw_audio[audioIdx % numSamples]) * sin(sin_phase)*amp;
+            // buf[i] = audios[audioIdx].raw_audio[audios[audioIdx].currentIdx % audios[audioIdx].numSamples];
+            // buf[i] = sin(sin_phase)*(float)(1<<15);
+            float v = (float)(audios[audioIdx].raw_audio[audios[audioIdx].currentIdx % audios[audioIdx].numSamples]) * audios[audioIdx].gain;
+            if (lerp_factor > 0){
+                v *= (1-lerp_factor);
+                v += (float)(audios[audioIdx + 1].raw_audio[audios[audioIdx + 1].currentIdx % audios[audioIdx + 1].numSamples]) * (lerp_factor)*audios[audioIdx + 1].gain;
+            }
+            v *= (1-SIN_AMMOUNT);
+            v += SIN_AMMOUNT * sin(sin_phase)*(float)INT16_MAX;
+            // buf[i] += (randn()*1000);
+            buf[i] = v;
+            
+            audios[audioIdx].currentIdx++;
+            audios[audioIdx + 1].currentIdx++;
+        }
+
+        /* ... and play it */
+        if (pa_simple_write(s, buf, sizeof(buf), &error) < 0)
+        {
+            fprintf(stderr, __FILE__ ": pa_simple_write() failed: %s\n", pa_strerror(error));
+            goto finish;
+        }
+    
+        // write to file
+        wave_write(of,buf,1024);
+    }
+
+    /* Make sure that every single sample was played */
+    if (pa_simple_drain(s, &error) < 0)
+    {
+        fprintf(stderr, __FILE__ ": pa_simple_drain() failed: %s\n", pa_strerror(error));
+        goto finish;
+    }
+
+    ret = 0;
+
+finish:
+
+    if (s)
+        pa_simple_free(s);
+    for (size_t i = 0; i < 4; i++)
+    {
+        free(audios[i].raw_audio);
+    }
+
+    return ret;
+}
